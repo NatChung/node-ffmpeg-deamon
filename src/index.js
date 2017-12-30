eebb44c38881d4de88607206917229d026da93da
Nat <nat@starvedia.com>
2017-12-30 21:07:35 +0800

    [M]Fixed bug

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 import { spawn } from 'child_process';
 
-console.log(`Read to go`)
-let ffmepg = spawn('ffmpeg', [
-    '-f', 'mpegts',
-    '-i', 'udp://127.0.0.1:12345',
-    '-codec', 'copy',
-    '-f', 'mpegts',
-    'udp://127.0.0.1:12346'
-])
-
-ffmepg.on('error', error => {
-    console.log(`ffmpeg error -->`, error)
-})
-
-ffmepg.on('exit', code => {
-    console.log(`Got exit code:`, code)
+let ffmepg = null
+const initFFmpeg = () => {
     ffmepg = spawn('ffmpeg', [
         '-f', 'mpegts',
         '-i', 'udp://127.0.0.1:12345',
         '-codec', 'copy',
         '-f', 'mpegts',
-        'udp://127.0.0.1:12346'
+        'udp://127.0.0.1:12346',
+        '-vcodec','copy',
+        '-an',
+        '-f', 'rtp',
+        'rtp://127.0.0.1:12347'
     ])
-})
 
-ffmepg.stdout.on('data', data => {
-    console.log(`Data: `, data)
-})
+    ffmepg.on('error', error => {
+        console.log(`ffmpeg error -->`, error)
+    })
+    
+    ffmepg.on('exit', code => {
+        initFFmpeg()
+    })
+    
+    ffmepg.stdout.on('data', data => {
+        console.log(`Data: `, data)
+    })
+}
+
+
+initFFmpeg()
